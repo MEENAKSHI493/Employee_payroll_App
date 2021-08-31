55f93ae5e9105c2a1dbe5a03ae17934600581f06
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-08-31 22:18:31 +0530

    Added:Employee payroll data

--- employee-payroll-jsonserver.js/js/payroll_form.js
+++ employee-payroll-jsonserver.js/js/payroll_form.js
@@ -5,3 +5,29 @@ output.textContent = salary.value;
 salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
+
+document.getElementById("submitButton").onclick = function () {
+    let Name = document.getElementById("name").value;
+    let Picture = document.querySelector("input[name = profile]:checked").value;
+    let Gender = document.querySelector("input[name = gender]:checked").value;
+    let Department = document.querySelector(
+      "input[name = department]:checked"
+    ).value;
+    let salary = document.getElementById("salary").value;
+    let notes = document.getElementById("notes").value;
+    let startDate = new Date(
+      parseInt(document.getElementById("year").value),
+      parseInt(document.getElementById("month").value),
+      parseInt(document.getElementById("day").value)
+    );
+    let employee = new EmployeeInfo(
+      Name,
+      Picture,
+      Gender,
+      Department,
+      Salary,
+      Notes,
+      StartDate
+    );
+    alert(employee.toString());
+  };
